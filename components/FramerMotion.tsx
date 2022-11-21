b9bfe9282826a0fc6d0903e42d32ba08aaace5fd
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-11-21 17:18:46 -0500

    ci: add framer-motion

---
+++ components/FramerMotion.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const FramerMotion = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+    >
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+      <h1 className="text-[2rem] font-[700]">Hello World</h1>
+    </motion.div>
+  );
+};
+
+export default FramerMotion;
