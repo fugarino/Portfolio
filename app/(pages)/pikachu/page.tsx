76ffdc2d6f59b453444701423f8c03d452454438
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-02-06 13:56:38 -0500

    fix: pikachu page types

--- app/(pages)/pikachu/page.tsx
+++ app/(pages)/pikachu/page.tsx
@@ -1,7 +1,6 @@
-import { NextPage } from "next";
 import ArrowLink from "../../components/ui/links/ArrowLink";
 
-const PikachuPage: NextPage = () => {
+const PikachuPage = () => {
   return (
     <div className="bg-project-3 min-h-screen overflow-x-hidden">
       <div className="max-w-[1320px] mx-auto px-10 pt-4">
