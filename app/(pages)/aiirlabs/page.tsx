83ab1de283c0aace2f984a593c18fc824adec169
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-03-11 13:04:28 -0500

    test: client side reload

--- app/(pages)/aiirlabs/page.tsx
+++ app/(pages)/aiirlabs/page.tsx
@@ -1,16 +1,15 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
 import ArrowLink from "../../components/ui/links/ArrowLink";
 import AiirlabsDesktop from "./AiirlabsDesktop";
 import AiirlabsMobile from "./AiirlabsMobile";
 
 const AiirlabsPage = () => {
-  useEffect(() => {
-    const body = document.querySelector("body");
-    if (!body) return;
-    body.style.backgroundColor = "#E9E2DB";
-  }, []);
+  // useEffect(() => {
+  //   const body = document.querySelector("body");
+  //   if (!body) return;
+  //   body.style.backgroundColor = "#E9E2DB";
+  // }, []);
 
   return (
     <div className="bg-[#E9E2DB] max-w-[1320px] mx-auto text-black min-h-screen overflow-x-hidden">
