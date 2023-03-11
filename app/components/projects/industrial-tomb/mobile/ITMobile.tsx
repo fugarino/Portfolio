e14d7ad02b524671ad068fb8cbbcb2e015da23b3
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-03-11 17:29:23 -0500

    test: testing ITVideo component not showing up in production build

--- app/components/projects/industrial-tomb/mobile/ITMobile.tsx
+++ app/components/projects/industrial-tomb/mobile/ITMobile.tsx
@@ -19,7 +19,7 @@ const ITMobile = () => {
           completion time: 3 stars for under 5 minutes, 2 stars for under 10
           minutes, and 1 star for simply finishing."
       />
-      <ITVideoSection
+      {/* <ITVideoSection
         src="MY Movie1"
         paragraph="For the majority of the game's visuals, including the character,
           a 16x16 tileset was used. The individual collections of tiles were
@@ -28,7 +28,7 @@ const ITMobile = () => {
           creating the illusion of movement and keeping the player centered at
           all times. As a result, the game offers a beautiful UI that adapts
           well to all screen sizes."
-      />
+      /> */}
       <div className="relative h-[15rem] sm:h-[20rem] rounded-lg bg-[#181a1c] overflow-hidden mx-[1rem] sm:mx-[2rem] mt-4">
         <div className="relative w-full">
           <div className="absolute h-full w-[40rem] sm:w-[60rem] -bottom-[2.2rem] left-[1.2rem]">
--- app/components/projects/industrial-tomb/mobile/ITVideoSection.tsx
+++ app/components/projects/industrial-tomb/mobile/ITVideoSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRef, useState } from "react";
-import ITVideoM from "./ITVideoM";
 
 const ITVideoSection = ({
   src,
@@ -12,6 +11,9 @@ const ITVideoSection = ({
 }) => {
   const [isCarouselLeft, setIsCarouselLeft] = useState(true);
   const carousel = useRef<HTMLDivElement>(null);
+
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const vidRef = useRef<HTMLVideoElement | null>(null);
 
   const handleNextClick = () => {
     carousel.current?.scrollBy({ left: 250, behavior: "smooth" });
@@ -40,7 +42,57 @@ const ITVideoSection = ({
           className="w-[calc(100%-3rem)] sm:w-[calc(100%-6rem)] max-w-[463px] h-[20rem] sm:h-[19rem]
 		 	    rounded-lg overflow-hidden shrink-0 snap-center snap-always"
         >
-          <ITVideoM src={src} isCarouselLeft={isCarouselLeft} />
+          {/* <ITVideoM src={src} isCarouselLeft={isCarouselLeft} /> */}
+          <div
+            onClick={() => {
+              if (isCarouselLeft) {
+                isVideoPlaying
+                  ? vidRef.current?.pause()
+                  : vidRef.current?.play();
+                setIsVideoPlaying((prevState) => !prevState);
+              }
+            }}
+            className="relative bg-[#121314] cursor-pointer h-full sm:h-[20rem] rounded-lg"
+          >
+            <div className="absolute text-white z-10 top-6 left-6 sm:top-8 sm:left-8">
+              {!isVideoPlaying ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )}
+            </div>
+            <video
+              muted
+              loop
+              playsInline
+              ref={vidRef}
+              className="w-full h-full object-cover rounded-lg"
+            >
+              <source src={`/${src}.mp4#t=0.001`} type="video/mp4" />
+            </video>
+          </div>
         </div>
         <div
           onClick={handleNextClick}
