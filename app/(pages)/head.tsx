601ac08622e2cb21221cf3f20dbdba13725c3758
Christian Fugarino <christian.fugarino.123@gmail.com>
2023-03-14 08:13:26 -0400

    ci: add metadata

--- app/(pages)/head.tsx
+++ app/(pages)/head.tsx
@@ -10,6 +10,14 @@ export default function Head() {
         name="description"
         content="Personal portfolio of Christian Fugarino"
       />
+      <meta name="title" property="og:title" content="Fugarino" />
+      <meta property="og:type" content="Website" />
+      <meta
+        name="image"
+        property="og:image"
+        content="https://live.staticflickr.com/65535/52746282082_443f1ba14d_k.jpg"
+      />
+      <meta name="author" content="Christian Fugarino" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
